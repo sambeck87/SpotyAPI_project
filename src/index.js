882c3f5b66fd5602a7ea4d35cf8389af2915ee40
sambeck87 <sandro_mipb@hotmail.com>
2022-12-23 09:02:49 -0600

    Setup webpack

---
+++ src/index.js
@@ -0,0 +1,2 @@
+import './style.css';
+import './img/damian_sastre.jpg'
---
+++ webpack.config.js
@@ -0,0 +1,37 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  mode: 'development',
+  entry: './src/index.js',
+  plugins: [
+    new MiniCssExtractPlugin(),
+    new HtmlWebpackPlugin({
+      template: './src/index.html',
+    }),
+  ],
+  output: {
+    filename: '[name].js',
+    path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: [{
+          loader: MiniCssExtractPlugin.loader,
+          options: { publicPath: path.relative(__dirname, '') },
+        },
+          'css-loader',
+        ],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/,
+        type: 'asset/resource',
+        generator: { filename: './img/[name][ext]' },
+      },
+    ],
+  },
+};
